f28ff2c6a7bfcd2455b4e7239ab6951ed62272ee
codingdie <codingdie@gmail.com>
2022-01-01 12:01:54 +0800

    fix coredump

--- src/core/StreamTunnel.cpp
+++ src/core/StreamTunnel.cpp
@@ -10,7 +10,7 @@ StreamTunnel::StreamTunnel(const string &type, const string &ip, int port, const
 StreamTunnel::StreamTunnel(const string &type, const string &area) : type(type), area(area) {}
 
 string StreamTunnel::toString() {
-    if ("DIRECT" == type) {
+    if (type.compare("DIRECT") == 0) {
         return "DIRECT";
     } else {
         return type + ":" + ip + ":" + to_string(port);
