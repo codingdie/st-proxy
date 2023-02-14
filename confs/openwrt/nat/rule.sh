01ba0768086aec0c47c58dc6eb621835571866c6
codingdie <codingdie@gmail.com>
2023-02-14 16:18:10 +0800

    optimize ip area query and support only http proxy

--- confs/openwrt/nat/rule.sh
+++ confs/openwrt/nat/rule.sh
@@ -12,8 +12,8 @@ if [ "$1" != "clean" ]; then
   iptables -t nat -N st-proxy
   iptables -t nat -A st-proxy -m set --match-set st-proxy-whitelist dst -j RETURN
   if [ "$2" != "" ]; then
-
-  iptables -t nat -A st-proxy -m multiport -p tcp ! --destination-port  443,80 -j RETURN
+    iptables -t nat -A st-proxy -m multiport -p tcp ! --destination-port  443,80 -j RETURN
+  fi
 #  iptables -t nat -A st-proxy -p tcp  -j LOG --log-prefix "st-proxy-all" --log-level 6
 #  iptables -t nat -A st-proxy -p tcp  -m mark --mark 1024 -j LOG --log-prefix "st-proxy-mark" --log-level 6
 
