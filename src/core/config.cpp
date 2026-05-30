c9471270aef81afb7987ed94628725095b177e5c
codingdie <codingdie@gmail.com>
2026-05-30 01:31:21 +0000

    fix(config): avoid logger shutdown during static destruction

--- src/core/config.cpp
+++ src/core/config.cpp
@@ -160,6 +160,4 @@ void config::parse_whitelist_to_ips() {
         tunnel->ip_whitelist = parse_whitelist_to_ips(tunnel->whitelist);
     }
 }
-config::~config() {
-    st::utils::logger::disable();
-}
+config::~config() = default;
